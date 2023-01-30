9574b8be7cf1152eddd9d987f50fc1eb8fb1094b
Ahalya5 <ahalyaahalya1213@gmail.com>
2023-01-30 11:53:09 +0530

    repository commit

--- src/main/java/com/app/matrimony/repository/BrokerCustomerRepository.java
+++ src/main/java/com/app/matrimony/repository/BrokerCustomerRepository.java
@@ -10,15 +10,15 @@ import com.app.matrimony.entity.BrokerCustomer;
 
 public interface BrokerCustomerRepository extends WriteableRepository<BrokerCustomer, UUID> {
 	
-	@Query(value = "Select * from broker_customer bc where bc.phone_no =:phoneNo ",nativeQuery = true)
+	@Query(value = "Select * from mat_broker_customer bc where bc.phone_no =:phoneNo ",nativeQuery = true)
 	public Optional<BrokerCustomer> findByPhoneNo(String phoneNo);
 	
 	Optional<BrokerCustomer> findByUserName(String name);
 	
-	@Query(value = "select * from broker_customer bc where bc.email=:email",nativeQuery = true)
+	@Query(value = "select * from mat_broker_customer bc where bc.email=:email",nativeQuery = true)
 	Optional<BrokerCustomer> findByEmail(String email);
 	
-	@Query(value = "select * from broker_customer bc where bc.role_type =:admin",nativeQuery = true)
+	@Query(value = "select * from mat_broker_customer bc where bc.role_type =:admin",nativeQuery = true)
 	Optional<BrokerCustomer> findByRoleType(String admin);
 	
 	Optional<BrokerCustomer> findByOtp(String otp);
